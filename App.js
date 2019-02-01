81a42d9f5116fcc83f8ed73ecd951218d05fc04f
Karolis <kar.zukas@gmail.com>
2019-02-01 23:03:06 +0200

    Login with firebase, error message handling

--- App.js
+++ App.js
@@ -1,77 +1,11 @@
-import React from 'react';
-import { ActivityIndicator, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
-import Firebase from './Firebase';
-import { Input } from './components/Input';
-import { Button } from './components/Button';
+import React, { Component } from 'react';
+import { View } from 'react-native'
+import AppNavigator from './components/AppNavigator';
 
-export default class App extends React.Component {
-  state = {
-    email: '',
-    password: '',
-    authenticating: false
-  }
-
-  componentWillMount() {
-    Firebase.init();
-  }
-
-  onPressSignIn() {
-    console.log(this.state);
-    this.setState({authenticating: true})
-  }
-
-  onPressRegister() {
-
-  }
-
-  renderCurrentState() {
-    if(this.state.authenticating){
-      return (
-        <View style={styles.form}>
-          <ActivityIndicator size='large' />
-        </View>
-      )
-    } else {
-      return (
-        <View style={styles.form}>
-          <Input 
-            placeholder='Enter your email'
-            label='Email'
-            onChangeText={email => this.setState({ email })}
-            value = {this.state.email}
-          />
-          <Input 
-          placeholder='Enter your password'
-          label='Password'
-          secureTextEntry
-          onChangeText={password => this.setState({ password })}
-          value = {this.state.password}
-          />
-          <Button onPress={() => this.onPressSignIn()}>Log in</Button>
-          <Button onPress={() => this.onPressRegister()}>Register</Button>
-        </View>
-      )
-    }
-  }
-
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        {this.renderCurrentState()}
-      </View>
-    );
+      <AppNavigator />
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row'
-  },
-  form: {
-    flex: 1
-  }
-});

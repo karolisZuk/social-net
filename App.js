2112ee50b29c59e611e1577af80540e3fb3bb03a
Karolis <kar.zukas@gmail.com>
2019-02-09 22:12:11 +0200

    2.5 pamoka login flow with loading animations and error messages

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { View } from 'react-native'
-import AppNavigator from './components/AppNavigator';
+import AppNavigator from './routes/AppNavigator';
 
 export default class App extends Component {
   render() {
     return (
+      // Navigatorius savyje laiko jame aprasytus komponentus, arba kitus navigatorius ir jiem perduoda kelis propsus
       <AppNavigator />
     )
   }

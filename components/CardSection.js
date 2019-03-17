429afa50b53df5de4e9cf9e82129c71ae00a99d0
Karolis <kar.zukas@gmail.com>
2019-03-17 17:09:36 +0200

    indentation

--- components/CardSection.js
+++ components/CardSection.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
 const CardSection = (props) => {
-  return (
-    <View style={styles.containerStyle}>
-        {props.children}
-    </View>
-  );
+    return (
+        <View style={styles.containerStyle}>
+            {props.children}
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
